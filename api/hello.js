0b0f2cb25eec3ac407027b9a9dc96e407143797b
greenstarxu <greenstarxu@users.noreply.github.com>
2026-02-20 11:06:53 +0400

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,6 @@
-// 最简单的版本，没有任何依赖
 module.exports = (req, res) => {
-    res.json({ 
-        message: 'API is working!',
+    res.json({
+        message: 'Hello World',
         time: new Date().toISOString()
     });
 }
